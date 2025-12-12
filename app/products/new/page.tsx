d75b60d08aa1f2f79b46364b62f799f07e4026ec
Mebishnupriya20 <bishnupriya.ss2005@gmail.com>
2025-12-12 10:06:03 +0000

    all product page done

---
+++ app/products/new/page.tsx
@@ -0,0 +1,22 @@
+import ProductForm from "@/components/ProductForm";
+
+export default function AddProductPage(){
+    async function handleAdd(formData: any) {
+    await fetch("https://dummyjson.com/products/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    alert("Product Added!");
+}
+
+return (
+    <div className="max-w-2xl mx-auto bg-white p-8 rounded-x1 shadow mt-10">
+        <h1 className="text-2xl font-bold mb-6">Add Products</h1>
+
+        <productForm submitText="Add Product" onSubmit={handleAdd} />
+        </div>
+
+);
+}
